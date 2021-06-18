16147d8e79e22de8132de331ad87c27a9c0d59c4
catherinecheng02 <z5310517@ad.unsw.edu.au>
2021-06-18 10:08:51 +1000

    Create while2.c

---
+++ tut03/while2.c
@@ -0,0 +1,15 @@
+// Tutorial Question 4 
+// While loop with error, example 2
+#include <stdio.h>
+
+int main(void) {
+    int i = 0;
+    int j = 0;
+
+    while (j == 1 || i < 100) {
+        printf("%d\n", i);
+        i = i + 1;
+    }
+
+
+}
