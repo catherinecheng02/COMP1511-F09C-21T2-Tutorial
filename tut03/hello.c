d2dbcc189d4a0997d9ae113110f14731352bf868
catherinecheng02 <z5310517@ad.unsw.edu.au>
2021-06-18 10:08:11 +1000

    Create hello.c

---
+++ tut03/hello.c
@@ -0,0 +1,14 @@
+// QUESTION 3
+// Program to print out "hello" ten times
+
+# include <stdio.h>
+
+int main(void) {
+    int i = 1;
+    while (i <= 10) {
+        printf("hello\n");
+        i++;
+    
+    }
+
+}
