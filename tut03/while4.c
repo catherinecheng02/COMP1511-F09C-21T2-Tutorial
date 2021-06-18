aba2b28a8b504caf7d7d2e55de1ad9527b41fd76
catherinecheng02 <z5310517@ad.unsw.edu.au>
2021-06-18 10:10:26 +1000

    Create while4.c

---
+++ tut03/while4.c
@@ -0,0 +1,12 @@
+// Tutorial Question 4 
+// While loop with error, example 4
+#include <stdio.h>
+
+int main(void) {
+    int i = 0;
+    while (i < 10)  {
+        printf("%d\n", i);
+        i = i + 1;
+    }
+    
+}
