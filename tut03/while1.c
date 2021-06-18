b298b58e8305be0d93ed4a63eddc3031ddc3c777
catherinecheng02 <z5310517@ad.unsw.edu.au>
2021-06-18 10:08:31 +1000

    Create while1.c

---
+++ tut03/while1.c
@@ -0,0 +1,15 @@
+// Tutorial Question 4 
+// While loop with error, example 1
+
+#include <stdio.h>
+
+int main(void) {
+    int i = 0;
+
+    while (i < 100) {
+        printf("%d\n", i);
+        i = i + 1;
+    }   
+
+
+}
