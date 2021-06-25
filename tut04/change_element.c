845284c90679d09550623f0d7c6e48db83befc3f
catherinecheng02 <z5310517@ad.unsw.edu.au>
2021-06-25 10:49:37 +1000

    Create change_element.c

---
+++ tut04/change_element.c
@@ -0,0 +1,27 @@
+// QUESTION 22
+// Here is a program change_element.c, what will this code print before and 
+// after the function is called? Is this output expected?
+
+#include <stdio.h>
+
+#define SIZE 1000
+
+void change_array_element(int arr[SIZE], int index);
+
+int main(void) {
+    int array[SIZE] = {0};
+
+    int index;
+    printf("Enter index to change: ");
+    scanf("%d", &index);
+
+    printf("Before function: %d\n", array[index]);
+    change_array_element(array, index);
+    printf("After function: %d\n", array[index]);
+}
+
+// Change an element of an array at a given index to
+// now have 42
+void change_array_element(int arr[SIZE], int index) {
+    arr[index] = 42;
+}
