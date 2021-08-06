20c9d70cbb49dc21af96d04728b069485b8fc821
catherinecheng02 <z5310517@ad.unsw.edu.au>
2021-08-06 10:20:51 +1000

    Create adt_headers_stack.c

---
+++ tut10/adt_headers_stack.c
@@ -0,0 +1,53 @@
+// Tutorial 10
+// Question 5 - 7 
+// Abstract Data Types, Header Files and Stacks
+// Written by Catherine Cheng (z5310517)
+
+// QUESTION 5
+
+// Examples of data types
+// - int, double, char
+
+// What is an Abstract Data Type?
+// - like an alias for a data typedef
+// - stores data, but we don't know how it stores data 
+
+// Examples?
+// - queues, stacks 
+
+// What is the purpose of a Header file?
+// - typedefs, function declarations only - implementation is hidden
+
+// What is the purpose of a Implementation file?
+// - implement all functions and typedefs
+
+
+
+
+
+// QUESTION 6
+// I have two files, a list.h and a list.c that describe a particular form of list that I want to use.
+
+// I then have a main.c that would like to use the functionality offered by the other two files.
+
+// Which files need to #include the other files?
+// - main.c and list.c need to #include "list.h"
+
+// What is the compilation command that will combine these files into a single program?
+// - dcc main.c list.c -o program_name
+// - only need to compile .c files 
+
+
+
+// QUESTION 7
+// What is a stack?
+
+// - example of abstract data type
+// - last in first out 
+
+
+
+
+
+
+
